1e42b85ceebf300fb2d01fcba566f40f07108356
matteofab10 <matteofabio10@gmail.com>
2021-11-10 14:56:34 +0100

    esercizio base

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+const app = new Vue({
+  // si inserisce il codice vue
+  // dico a Vue dove deve puntare l'app
+  el: '#app',
+  // in data inserisco tutte le variabili e saranno delle proprietà di data
+  data: {
+    saluto: 'Hello World !'
+  }
+
+
+
+
+})
